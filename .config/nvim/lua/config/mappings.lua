bb61863d265a4b9c432cb51801bc0b5058701661
M3nny <m3nny@protonmail.com>
2025-03-31 15:27:55 +0200

    Add a function to show virtual lines for errors

--- .config/nvim/lua/config/mappings.lua
+++ .config/nvim/lua/config/mappings.lua
@@ -10,3 +10,19 @@ vim.keymap.set("i", "kj", "<Esc>", opts) -- map <kj> to <Esc>
 vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- hover docs
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- go to definition
 vim.keymap.set("n", "rn", vim.lsp.buf.rename, opts) -- rename the variable under the cursor across files
+vim.keymap.set("n", "ca", vim.lsp.buf.code_action, opts) -- show code actions
+
+local function toggle_virtual_lines()
+	--@type boolean|table
+	local current_virtual_lines_config = vim.diagnostic.config().virtual_lines
+
+	if current_virtual_lines_config then
+		-- disable virtual lines if they were previously enabled
+		vim.diagnostic.config({ virtual_lines = false })
+	else
+		-- enable virtual lines if they were previously disabled
+		vim.diagnostic.config({ virtual_lines = { current_line = true } })
+	end
+end
+
+vim.keymap.set("n", "L", toggle_virtual_lines, opts) -- toggle virtual lines
