d12b5dacff1d4c1c518c7ebd08136855def4945d
M3nny <77993658+M3nny@users.noreply.github.com>
2025-05-30 22:06:55 +0200

    Fix scope indent highlight

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -7,20 +7,23 @@ return {
 
 	main = "ibl",
 
-	opts = {
-		scope = {
-			show_start = false,
-			show_end = false,
-		},
+	config = function()
+		require("ibl").setup({
+			scope = {
+				enabled = true,
+				show_start = false,
+				show_end = false,
+			},
 
-		indent = {
-			char = "│",
-			tab_char = "│",
-			smart_indent_cap = true,
-		},
+			indent = {
+				char = "│",
+				tab_char = "│",
+				smart_indent_cap = true,
+			},
 
-		whitespace = {
-			remove_blankline_trail = true,
-		},
-	},
+			whitespace = {
+				remove_blankline_trail = true,
+			},
+		})
+	end,
 }
