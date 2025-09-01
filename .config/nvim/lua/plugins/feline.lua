49f25879ea2d398e3e0e84d7ccfde2977522d026
M3nny <77993658+M3nny@users.noreply.github.com>
2025-09-01 09:18:49 +0200

    Update feline statusbar ctp setup method

--- .config/nvim/lua/plugins/feline.lua
+++ .config/nvim/lua/plugins/feline.lua
@@ -5,8 +5,9 @@ return {
 	config = function()
 		-- set feline as statusline with catppuccin theme
 		local ctp_feline = require("catppuccin.groups.integrations.feline")
+		ctp_feline.setup()
 		require("feline").setup({
-			components = ctp_feline.get(),
+			components = ctp_feline.get_statusline(),
 		})
 		vim.cmd("set noshowmode")
 	end,
