0b1b606ea4d4c95e3057d7b6a4570bc9bb13ad8b
M3nny <m3nny@protonmail.com>
2025-12-03 16:06:24 +0100

    fix(nvim): start treesitter for the current buffer

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -25,4 +25,24 @@ return {
 		highlight = { enable = true },
 		indent = { enable = true, disable = { "python" } },
 	},
+
+	config = function(_, opts)
+		-- Setup Treesitter
+		require("nvim-treesitter.configs").setup(opts)
+
+		-- Start Treesitter automatically for buffers that have a parser installed
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				local buf = vim.api.nvim_get_current_buf()
+				local ft = vim.bo[buf].filetype
+
+				-- Only start Treesitter if a parser exists for this filetype
+				if require("nvim-treesitter.parsers").has_parser(ft) then
+					if not vim.treesitter.highlighter.active[buf] then
+						vim.treesitter.start(buf)
+					end
+				end
+			end,
+		})
+	end,
 }
