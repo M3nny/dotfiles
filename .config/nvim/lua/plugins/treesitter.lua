a410aabf537cc920f1bbfc1fd8763d19f1d07e6f
M3nny <m3nny@protonmail.com>
2026-04-07 08:43:52 +0200

    feat(nvim): use treesitter manager instead of the archived nvim-treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,50 +1,11 @@
--- Highlight, edit, and navigate code
+-- Manage tree-sitter parsers
 return {
-	"nvim-treesitter/nvim-treesitter",
+	"romus204/tree-sitter-manager.nvim",
 
-	-- Load when creating or opening a file
-	event = { "BufNewFile", "BufReadPre" },
+    -- Load when the parser manager is called
+	cmd = "TSManager",
 
-	opts = {
-		-- Add languages to be installed here that you want installed for treesitter
-		ensure_installed = {
-			"vim",
-			"vimdoc",
-			"lua",
-			"c",
-			"cpp",
-			"rust",
-			"python",
-			"javascript",
-			"typescript",
-			"html",
-			"css",
-			"just",
-			"json",
-			"toml",
-			"terraform",
-		},
-		highlight = { enable = true },
-		indent = { enable = true, disable = { "python" } },
-	},
-
-	config = function(_, opts)
-		-- Setup Treesitter
-		require("nvim-treesitter.configs").setup(opts)
-
-		-- Start Treesitter automatically for buffers that have a parser installed
-		vim.api.nvim_create_autocmd("FileType", {
-			callback = function()
-				local buf = vim.api.nvim_get_current_buf()
-				local ft = vim.bo[buf].filetype
-
-				-- Only start Treesitter if a parser exists for this filetype
-				if require("nvim-treesitter.parsers").has_parser(ft) then
-					if not vim.treesitter.highlighter.active[buf] then
-						vim.treesitter.start(buf)
-					end
-				end
-			end,
-		})
+	config = function()
+		require("tree-sitter-manager").setup()
 	end,
 }
